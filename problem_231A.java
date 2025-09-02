499b6344790d2f841c4ef37631d7acfda7a6936a
Advik Banerjee <110986261+AdiJee2195@users.noreply.github.com>
2025-09-02 18:58:23 +0530

    Add files via upload

---
+++ problem_231A.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class problem_231A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int nq = sc.nextInt();
+        int count = 0;
+        int solve = 0;
+        for (int i = 0; i < nq; i++){
+            for (int j = 0; j < 3; j++){
+                    int m = sc.nextInt();
+                    if (m == 1) count++;
+                 }
+            if (count >= 2) solve++;
+            count = 0;
+        }
+        System.out.println(solve);
+    } 
+}
