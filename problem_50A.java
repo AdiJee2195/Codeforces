da93e82707de656198d3f56489929697e8222386
Advik Banerjee <110986261+AdiJee2195@users.noreply.github.com>
2025-09-04 19:13:18 +0530

    Domino Piling

---
+++ problem_50A.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class problem_50A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int ns = n*m;
+
+        int nd = ns/2;
+
+        System.out.println(nd);
+    }
+}
